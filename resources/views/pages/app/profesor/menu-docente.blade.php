3cbc2e58507e7873db5a1132c4c3a65749f369f0
Pamela1994 <pamelaencima1994@gmail.com>
2020-10-12 21:03:46 -0300

    modificacion pamela

---
+++ resources/views/pages/app/profesor/menu-docente.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-light bg-dark">
+    <a class="navbar-brand">Sección Docente</a>
+    <ul class="nav justify-content-right">
+        <li>
+            <a class="nav-link text-white"><b>Ciclo Lectivo:</b></a>
+        </li>
+        <li>
+            <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1"
+                tabindex="-1" aria-hidden="true">
+                <option selected="selected" data-select2-id="3">2020 - Actual</option>
+                <option data-select2-id="30">2019</option>
+                <option data-select2-id="31">2018</option>
+                <option data-select2-id="31">2017</option>
+            </select>
+        </li>
+    </ul>
+</nav>
